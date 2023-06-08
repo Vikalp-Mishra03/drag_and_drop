902c8483bf2a241d4d1cbd4dbdfa341f4480450f
Vikalp Mishra <mishravikalp896@gmail.com>
2023-06-08 17:01:12 +0530

    use hide and hold

--- index.js
+++ index.js
@@ -3,12 +3,31 @@ const boxes = document.getElementsByClassName('box')
 
 // event listeners for draggable element box
 
-imgbox.addEventListener('dragstart', ()=> {
+imgbox.addEventListener('dragstart', (e)=> {
     // what happen when we grab the image
     console.log("dragstart has been triggered");
+    e.target.className += ' hold'
+    setTimeout(() => {
+        e.target.className += ' hide'
+    })
+    
 })
 imgbox.addEventListener('dragend', ()=> {
     // what happen when we leave the image
     console.log("dragend has been triggered");
 })
 
+for (box of boxes) {
+    box.addEventListener('dragover', ()=>{
+        
+    })
+    box.addEventListener('dragenter', ()=>{
+
+    })
+    box.addEventListener('dragleave', ()=>{
+
+    })
+    box.addEventListener('drop', ()=>{
+
+    })
+}
