cf880a64140db42e64ea6eb9f301bd45aa925868
Vikalp Mishra <mishravikalp896@gmail.com>
2023-06-08 17:07:49 +0530

    append the image in another

--- index.js
+++ index.js
@@ -3,31 +3,34 @@ const boxes = document.getElementsByClassName('box')
 
 // event listeners for draggable element box
 
-imgbox.addEventListener('dragstart', (e)=> {
+imgbox.addEventListener('dragstart', (e) => {
     // what happen when we grab the image
     console.log("dragstart has been triggered");
     e.target.className += ' hold'
     setTimeout(() => {
         e.target.className += ' hide'
     })
-    
+
 })
-imgbox.addEventListener('dragend', ()=> {
+imgbox.addEventListener('dragend', (e) => {
     // what happen when we leave the image
     console.log("dragend has been triggered");
+    e.target.className = 'imgbox'
 })
 
 for (box of boxes) {
-    box.addEventListener('dragover', ()=>{
-        
+    box.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        console.log('dragOver has been triggered')
     })
-    box.addEventListener('dragenter', ()=>{
-
+    box.addEventListener('dragenter', () => {
+        console.log('dragenter has been triggered')
     })
-    box.addEventListener('dragleave', ()=>{
-
+    box.addEventListener('dragleave', () => {
+        console.log('dragleave has been triggered')
     })
-    box.addEventListener('drop', ()=>{
-
+    box.addEventListener('drop', (e) => {
+        console.log('drop has been triggered')
+        e.target.append(imgbox);
     })
 }
